113181eb64e138c552a30195aa8544c776a16fed
ilevashov <igor.Levashov95@mail.ru>
2020-05-14 00:23:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+let defaultNumber = 0; 
+let isDec = false; 
+ 
+function inc(n) 
+{ 
+n = n+1; 
+console.log ("число увеличивается на 1", n); 
+ 
+defaultNumber = n; 
+} 
+ 
+function dec (n) 
+{ 
+n = n-1; 
+console.log ("число уменьшено на 1", n); 
+ 
+defaultNumber = n; 
+} 
+ 
+function checkofclick (n) 
+ { 
+ if (isDec) 
+  { 
+  if (n !== 0) 
+   { 
+   dec(n); 
+   } 
+  else 
+   { 
+   isDec = false; 
+   inc (n); 
+   }  
+  } 
+  
+ else 
+  { 
+  console.log (n<10)  
+  if (n<10) 
+   { 
+   inc(n); 
+   } 
+  else if (n>=10) 
+   { 
+   isDec = true 
+   dec (n)   
+   } 
+  }
+  let item = 0;
+
+  while (item < 5) 
+    $('#itemlist').html(`<li></li>`} 
+ }
