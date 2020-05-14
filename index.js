0f51215b50392d339c4a528b861751ea06701243
ilevashov <igor.Levashov95@mail.ru>
2020-05-14 22:58:52 +0300

    message

--- index.js
+++ index.js
@@ -1,61 +1,78 @@
-let defaultNumber = 0; 
-let isDec = false; 
- 
-function inc(n) 
-{ 
-n = n+1; 
-console.log ("число увеличивается на 1", n); 
- 
-defaultNumber = n; 
-} 
- 
-function dec (n) 
-{ 
-n = n-1; 
-console.log ("число уменьшено на 1", n); 
- 
-defaultNumber = n; 
-} 
- 
-function checkofclick (n) 
- { 
- if (isDec) 
-  { 
-  if (n !== 0) 
-   { 
-   dec(n); 
-   } 
-  else 
-   { 
-   isDec = false; 
-   inc (n); 
-   }  
-  } 
-  
- else 
-  { 
-  console.log (n<10)  
-  if (n<10) 
-   { 
-   inc(n); 
-   } 
-  else if (n>=10) 
-   { 
-   isDec = true 
-   dec (n)   
-   } 
-  }
-  let item = 0;
+let defaultNumber = 0;
+let isDec = false;
+function inc(n) {
+    n = n + 1;
+    console.log("число увеличивается на 1", n);
+    defaultNumber = n;
+}
+function dec(n) {
+    n = n - 1;
+    console.log("число уменьшено на 1", n);
+    defaultNumber = n;
+}
+function checkofclick(n) {
+    if (isDec) {
+        if (n !== 0) {
+            dec(n);
+        } else {
+            isDec = false;
+            inc(n);
+        }
+    } else {
+        if (n < 10) {
+            inc(n);
+        } else if (n >= 10) {
+            isDec = true
+            dec(n)
+        }
+    }
+}
+let item = 0;
+function list(item) {
+    while (item < 10) {
+        $("#itemlist").append(`<li> тут должно было быть число </li>`);
+        item++;
+        console.log($item)
+    }
+}
+function addElement() {
+    let text = prompt('Введите название элемента')
+    console.log(text);
 
-  function list(item)
-  {
+    let html = `
+      <li>
 
-   while (item < 10) 
-   {
-     $('#itemlist').append(`<li></li>`);
-     item++;
-     console.log(item) 
-   }
-  }
+        <div class"itemlist">
+          <span class='target' onmouseenter="toggleColor(this, true)" onmouseleave="toggleColor(this)">
+            ${text}
+          </span>
+        <button class="delete" onclick="deleteElement(this.parentNode.parentNode)">x</button>
 
- }
+          </div>
+      </li>`;
+
+      $('#itemlist').append(html);
+}
+
+function deleteElement(element) {
+
+    console.log("Удалить элемент");
+    console.log(element);
+
+    $(element).replaceWith();
+}
+
+function toggleColor(element, active = false) {
+    console.log(element, active);
+
+    let activeColor = 'red';
+    let defaultColor = '#333';
+
+     if (active) {
+        $(element).css('color', activeColor);
+    } else {
+        $(element).css('color', defaultColor);
+    }
+
+}
+
